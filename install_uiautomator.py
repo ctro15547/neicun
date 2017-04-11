ea9208167f2b9b2a89f59fd507ed100e7fa3c525
或许叫焕民-公司 <2656117195@qq.com>
2017-04-11 17:51:31 +0800

    修改成python -m pip

--- install_uiautomator.py
+++ install_uiautomator.py
@@ -6,11 +6,11 @@ opts, args = getopt.getopt(sys.argv[1:], "hp:t:c:d:")
 try:
 	import urllib3
 except:
-	child = subprocess.Popen('pip install urllib3 -i https://pypi.douban.com/simple')
+	child = subprocess.Popen('python -m pip install urllib3 -i https://pypi.douban.com/simple')
 	child.wait()
 
 try:
 	import uiautomator
 except:
-	child = subprocess.Popen('pip install uiautomator -i https://pypi.douban.com/simple')
+	child = subprocess.Popen('python -m pip install uiautomator -i https://pypi.douban.com/simple')
 	child.wait()
