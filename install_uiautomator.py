c2ee452bb90863fa39a50237d9f43392a3f3fa79
或许叫焕民-公司 <2656117195@qq.com>
2017-04-12 14:07:00 +0800

    修改pip源地址到本地

--- install_uiautomator.py
+++ install_uiautomator.py
@@ -6,11 +6,11 @@ opts, args = getopt.getopt(sys.argv[1:], "hp:t:c:d:")
 try:
 	import urllib3
 except:
-	child = subprocess.Popen('python -m pip install urllib3 -i https://pypi.douban.com/simple')
+	child = subprocess.Popen('python -m pip install urllib3 --trusted-host 10.75.22.30 -i http://10.75.22.30:8088/123/')
 	child.wait()
 
 try:
 	import uiautomator
 except:
-	child = subprocess.Popen('python -m pip install uiautomator -i https://pypi.douban.com/simple')
+	child = subprocess.Popen('python -m pip install uiautomator --trusted-host 10.75.22.30 -i http://10.75.22.30:8088/123/')
 	child.wait()
