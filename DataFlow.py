6a113f179243dd407ceb25eed330bfc8a04bfcb6
或许叫焕民-公司 <2656117195@qq.com>
2017-04-20 12:02:44 +0800

    修改流量数据为star-end，加入保存获取的数据

--- DataFlow.py
+++ DataFlow.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
+import re
+import os
+import time
 def APP_traffic(App_name):
-	import re
-	import os
 	if  App_name.find(':') != -1 or App_name.find('/') != -1:
 		print 'error'
 		raise ValueError('App_name error')
@@ -33,12 +34,25 @@ def APP_traffic(App_name):
 					'Pid':Pid,
 					'Uid':Uid    }
 	return data_dict
+def link():
+	#Check the connection status
+	link = ''.join(os.popen('adb devices').readlines()[1])
+	#print link
+	try:
+		assert link.find('device') !=- 1,u'error'
+	except:
+		print 'off line'
+		raise ValueError('off line')
+
 
 if __name__ == '__main__':
 
-	import time
-	name = 'com.netease.cloudmusic'
-	t = 180
+	all_snd = []
+	all_rcv = []
+
+	name = 'com.tencent.tmgp.sgame'
+	t = 600
+	
 	try:
 		import sys,getopt
 		opts, args = getopt.getopt(sys.argv[1:], "hp:t:c:d:l:")
@@ -49,23 +63,58 @@ if __name__ == '__main__':
 			if op == '-t':
 				#print value
 				t = value
+			if op == '-l':
+				path1 = value + 'data_%s'%( str(time.strftime("%m%d", time.localtime())))
 	except:
 		raise ValueError('error')
+	
+	try:
+		os.makedirs('%s'%(path1))
+	except:
+		pass
+	
 	print name
+
 	print 'start!'
+
+	#Check the connection status
+	link()
+
 	a = APP_traffic(name)
 	start_snd = float(a['tcp_snd']) / 1024
 	start_rcv = float(a['tcp_rcv']) / 1024
-	print 'start_tcp_snd: %.2f KB'%(start_snd)
-	print 'start_tcp_rcv: %.2f KB'%(start_rcv)
+	time.sleep(1)
+	for T in range(int(t)):
 
-	time.sleep(int(t))
+		try:
+			link()
+		except:
+			break
 
-	a = APP_traffic(name)
-	end_snd = float(a['tcp_snd']) / 1024
-	end_rcv = float(a['tcp_rcv']) / 1024
-	print 'end_tcp_snd: %.2f KB'%(end_snd)
-	print 'end_tcp_rcv: %.2f KB'%(end_rcv)
-	print '**'*20
-	print 'tcp_snd: %.2f KB'%(end_snd - start_snd)
-	print 'tcp_rcv: %.2f KB'%(end_rcv - start_rcv)
+		print T+1
+		a = APP_traffic(name)
+		end_snd = float(a['tcp_snd']) / 1024
+		end_rcv = float(a['tcp_rcv']) / 1024
+
+		all_snd.append(float('%.2f'%(end_snd - start_snd)))
+		all_rcv.append(float('%.2f'%(end_rcv - start_rcv)))
+
+		print 'tcp_snd:' ,'%.2f'%(end_snd - start_snd) ,'KB'
+		print 'tcp_rcv:' ,'%.2f'%(end_rcv - start_rcv) ,'KB'
+
+		start_snd = end_snd 
+		start_rcv = end_rcv
+
+		time.sleep(0.5)
+
+	print 'snd + ',sum(all_snd)
+	print 'rcv + ',sum(all_rcv)
+		
+	print '生成数据文件...'
+	tiems = time.strftime("%m%d-%H%M%S", time.localtime())
+	File = open( r'%s'%(path2) + r'\Data_Flow_Data%s.txt'%(tiems),'w')
+	#File = open( 'c:\Data_Flow_Data%s.txt'%(tiems) ,'w')
+	File.write( 'SND' + ' ' + 'RCV' + '\n' )
+	for i in range( len(all_snd) ):
+		File.write( str(all_snd[i]) + ';' + str(all_rcv[i]) + '\n' )
+	File.close()
